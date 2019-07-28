d8c6e5e035d5e5b7dfa8d90cb2a8276f8e77bae3
Olli-Pekka Wallin <hozmaster@users.noreply.github.com>
2019-07-28 21:52:12 +0300

    Update post.pp

--- manifests/post.pp
+++ manifests/post.pp
@@ -1,11 +1,11 @@
 # default pre fw rule 
 class bs_firewall::post {
 
-  # drop all by default
-  firewall { '999 drop all':
-    proto  => 'all',
-    action => 'drop',
-    before => undef,
-  }
+    # drop all by default
+    firewall { '999 drop all':
+        proto  => 'all',
+        action => 'drop',
+        before => undef,
+    }
 
 }
